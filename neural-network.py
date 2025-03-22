76fc98d61422b7098571eeb5a7ccd07403582994
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2025-03-22 16:39:28 +0000

    FEAT: Implemented the feed forward algorithm

--- neural-network.py
+++ neural-network.py
@@ -10,7 +10,7 @@ class NeuralNetwork():
     def __init__(self, shape):
         
         """Shape of the neural network, defined as an array of layer sizes. 
-        For example, a neural network with five x variables, a hidden layer with three neurons, and two output nodes would be: [5,3,2]"""
+        For example, a neural network with five input variables, a hidden layer with three neurons, and two output nodes would be: [5,3,2]"""
         self.shape = shape
         self.layer_count = len(self.shape)
 
@@ -24,9 +24,26 @@ class NeuralNetwork():
         
         return
 
-    def feedforward():
-        """TO-DO: Feeds an input through the neural network to obtain a predicted output. Returns..."""
-    
+    def feedforward(self, input, activation_function):
+        """Feeds an input through the neural network to obtain a predicted output. 
+        Input shape must be (self.shape[0],1).
+        Activation function can be relu or sigmoid.
+        Returns a shape (self.shape[-1],1) output vector"""
+        # Validate activation function
+        if activation_function not in ["relu","sigmoid"]:
+            raise ValueError("Activation function must be 'relu' or 'sigmoid'")
+        
+        # Feed forward input through each layer of the neural network
+        activation = input
+        for bias, weight in zip(self.biases, self.weights):
+            if activation_function == "relu":
+                activation = relu(np.dot(weight, activation) + bias)
+            elif activation_function == "sigmoid":
+                activation = sigmoid(np.dot(weight, activation) + bias)
+        
+        # Return the output activation
+        return activation
+
     def sgd():
         """TO-DO: Trains the neural network using the stochastic gradient descent algorithm"""
         raise NotImplementedError()
@@ -35,3 +52,11 @@ class NeuralNetwork():
         """TO-DO: Uses the backpropagation algorithm to calculate """
         raise NotImplementedError()
     
+###ACTIVATION FUNCTIONS###
+def relu(input):
+    """Relu function"""
+    return max(0, input)
+
+def sigmoid(input):
+    """Sigmoid function"""
+    return 1.0/(1.0+np.exp(-input))
