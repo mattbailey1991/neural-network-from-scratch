9e7fbfc5d0341f1af37d5e7ad831f92abf8744da
Matthew Bailey <matthew.a.bailey@hotmail.co.uk>
2025-03-22 15:26:11 +0000

    First commit

---
+++ neural-network.py
@@ -0,0 +1,17 @@
+import random
+import numpy
+
+class NeuralNetwork():
+
+    def __init__(self, layer_size):
+        return
+    
+    
+    def sgd():
+        """TO-DO: Trains the neural network using the stochastic gradient descent algorithm"""
+        raise NotImplementedError()
+    
+    def backprop():
+        """TO-DO: Uses the backpropagation algorithm to calculate """
+        raise NotImplementedError()
+    
